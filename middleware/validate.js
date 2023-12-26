ebcde8bfcddf87ea3d8292dea20c8105b92a6a1d
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-26 08:16:08 +0500

    Add validation middleware

---
+++ middleware/validate.js
@@ -0,0 +1,10 @@
+import { validationResult } from "express-validator";
+
+export const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).send({ errors: errors.array() });
+    } else {
+        next();
+    }
+}
