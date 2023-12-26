a6416ebb42006878e0eae9feb144692387eb7176
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-26 14:48:10 +0500

    Refactor auth controller to use async/await

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,46 +1,52 @@
-const user = require('../models/userdata');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userdata'); // Assuming your model file is named userdata.js
+const bcrypt = require('bcrypt');
 
-exports.login = (req, res) => {
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-        return;
-    }
-    const email = req.body.email;
-    user.login(email, (err, data) => {
-        if (err) {
-            res.status(500).send({
-                message: err.message || "Some error occurred while login."
-            });
-        } else {
-            res.send(data);
-        }
-    }
-    );
-}
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-exports.register = (req, res) => {
-    
-        if (!req.body) {
-            res.status(400).send({
-                message: "Content can not be empty!"
-            });
-            return;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
         }
-        const user = {
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        };
-        user.register(user, (err, data) => {
-            if (err) {
-                res.status(500).send({
-                    message: err.message || "Some error occurred while register."
-                });
-            } else {
-                res.send(data);
-            }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
         }
-        );
+
+        const token = await user.generateAuthToken();
+        console.log(token)
+        res.cookie('jwt', token, { httpOnly: true, secure: false });
+        res.status(200).json({ message: 'User logged in successfully',
+        token:token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
     }
+};
+
+const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email already exists' });
+        }
+
+        const user = new User({ name, email, password });
+        await user.save();
+
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+module.exports = { login, register };
