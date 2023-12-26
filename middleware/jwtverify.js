eb2ca9457d1a43fc3eb307b44a215f228f29b2eb
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-26 20:52:27 +0500

    Refactor JWT authentication middleware to use authorization header instead of cookie

--- middleware/jwtverify.js
+++ middleware/jwtverify.js
@@ -2,18 +2,14 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-    const token = req.cookies.auth-token;
-    console.log("verv   "+token);
-
+    const token = req.headers['authorization'];
     if (!token) {
         return res.status(401).json({ message: 'Unauthorized' });
     }
-
     jwt.verify(token, process.env.JWT_KEY, (err, user) => {
         if (err) {
             return res.status(403).json({ message: 'Forbidden' });
         }
-
         req.user = user;
         next();
     });
